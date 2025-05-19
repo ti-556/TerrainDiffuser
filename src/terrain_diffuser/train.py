20c2d389b46906b88dabb66362b46666ae09f17e
TI <131604147+ti-556@users.noreply.github.com>
2025-05-19 20:39:03 +0900

    Delete src/terrain_diffuser directory

--- src/terrain_diffuser/train.py
+++
@@ -1,7 +0,0 @@
-import argparse
-from terrain_diffuser.training.trainer import run_from_yaml
-
-if __name__ == "__main__":
-    p = argparse.ArgumentParser()
-    p.add_argument("config", help="Path to YAML config")
-    run_from_yaml(p.parse_args().config)
